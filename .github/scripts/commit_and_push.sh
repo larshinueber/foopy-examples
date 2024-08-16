9e1faa878abc44123e101f486be18b9b6632fcbd
Lars Hinüber <105521415+larshinueber@users.noreply.github.com>
2024-08-16 12:40:57 +0200

    debug statement

--- .github/scripts/commit_and_push.sh
+++ .github/scripts/commit_and_push.sh
@@ -7,6 +7,7 @@ git status
 
 # check if there are any changes
 if [[ $(git diff --numstat | wc -l) > 0 ]]; then 
+echo "Changes detected. Committing and pushing."
 git add .
 git commit -m "Auto-update foo-space examples"
 git push --set-upstream origin $BRANCH_NAME
